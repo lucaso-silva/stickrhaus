106408d3a7dfdbb00ba2c0de8f67a3f5940ead85
Lucas Oliveira <luc.osilva@gmail.com>
2025-06-07 11:19:43 -0700

    Use stack component in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,12 @@
 import logo from '../img/logo.png';
-import {Button, Container, Col, Row} from 'react-bootstrap';
+import {Button, Container, Col, Row, Stack} from 'react-bootstrap';
 
-export default function Header(){
+export default function Header() {
     return (
-        <Container fluid>
-            <Row>
-                <Col>
-                    <h1><img src={logo} width="300px" alt="StickrHaus logo"/></h1>
-                </Col>
-                <Col>
-                    <Button variant="warning">Login</Button>
-                    <i className="bi bi-cart2 fs-3 ms-3"></i>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <hr className="border border-primary border-2 opacity-75"/>
-                </Col>
-            </Row>
-        </Container>
+        <Stack direction="horizontal" gap={1}>
+            <div className="ms-4"><h1><img src={logo} width="230px" alt="StickrHaus logo"/></h1></div>
+            <div className="ms-auto"><Button variant="warning">Login</Button></div>
+            <div className="me-5"><i className="bi bi-cart2 fs-3 ms-3"></i></div>
+        </Stack>
     )
 }
