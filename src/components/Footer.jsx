91c451f2c41dff998b0b2344742f39190e8493ee
Lucas Oliveira <luc.osilva@gmail.com>
2025-08-03 20:25:32 -0700

    Improve the footer responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,11 @@ export default function Footer(){
             <Container fluid>
                 <Row>
                     <Col>
-                        <Stack direction="horizontal" gap={5} className="p-3">
-                            <div><img src={logo} alt="Logo Stickerhaus" width="150px" /></div>
-                            <div className="ms-auto">
+                        <Stack gap={4} className="p-3 flex-column flex-sm-row justify-content-center align-items-center">
+                            <div>
+                                <img src={logo} alt="Logo Stickerhaus" width="150px" />
+                            </div>
+                            <div className="ms-sm-auto text-center my-2">
                                 <h5>Legal</h5>
                                 <ul className="links-footer">
                                     <li><a href="#">Privacy Policy</a></li>
@@ -19,22 +21,20 @@ export default function Footer(){
                             </div>
                             <div>
                                 <h5>Follow us</h5>
-                                <ul className="links-footer">
-                                    <li><i className="bi bi-instagram"></i></li>
-                                    <li><i className="bi bi-facebook"></i></li>
+                                <ul className="links-footer text-center">
+                                    <li><a href="#"><i className="bi bi-instagram"></i></a></li>
+                                    <li><a href="#"><i className="bi bi-facebook"></i></a></li>
                                 </ul>
                             </div>
                         </Stack>
                     </Col>
                 </Row>
                 <hr/>
-                <Row className="pb-2">
-                    <Col>Developed by Lucas Oliveira</Col>
+                <Row className="pb-2 px-3">
+                    <Col className="text-center">Developed by Lucas Oliveira</Col>
                     <Col>&copy; Stickrhaus</Col>
                 </Row>
             </Container>
-
-
         </>
     )
 }
